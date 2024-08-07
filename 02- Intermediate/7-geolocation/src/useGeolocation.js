91b70853642017468007bdd3e35f4dfa1bfb817a
Justin Amehunke <myschool22ml@gmail.com>
2024-08-07 13:46:17 +0000

    custom hook challenge

---
+++ 02- Intermediate/7-geolocation/src/useGeolocation.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function useGeolocation() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [position, setPosition] = useState({});
+
+
+    function getPosition() {
+
+        if (!navigator.geolocation)
+            return setError("Your browser does not support geolocation");
+
+        setIsLoading(true);
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                setPosition({
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude
+                });
+                setIsLoading(false);
+            },
+            (error) => {
+                setError(error.message);
+                setIsLoading(false);
+            }
+        );
+    }
+    return { isLoading, error, position, getPosition };
+}
+
+export default useGeolocation
